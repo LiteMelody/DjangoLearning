60373626063d7f2c1dfcca170df0cde7e8e997fb
Henry <xuehaozhong@gmail.com>
2023-12-12 23:01:53 -0500

    improve

--- projects/views.py
+++ projects/views.py
@@ -1,30 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'E website',
-        'description': 'a website'
-    },
-    {
-        'id': '2',
-        'title': 'lol website',
-        'description': 'a game website'
-    }
-]
+from .models import Project
 
 def projects(request):
-    msg = 'Successfully'
-    number = 11
-    context = {'number' : number, 'msg' : msg, 'projects':projectsList}
+    projects = Project.objects.all()
+    context = {'projects':projects}
     return render(request,'projects/projects.html', context)
 
 def project(request,pk):
-    projectObj = None 
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request,'projects/project.html',{'project':projectObj})
+
+def createProject(request):
+    context = {}
+    return render(request,'projects/project_form.html',context)
 
 # Create your views here.
